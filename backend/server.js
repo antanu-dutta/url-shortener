90a3ec84455f0e6e6f4ef04c8c786e4d5c4fce14
Antanu Dutta <140308923+antanu-dutta@users.noreply.github.com>
2025-09-15 16:16:19 +0530

    changed cors origin in server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", ""],
+    origin: ["http://localhost:5173", "https://url-shortener-gut2.onrender.com"],
     credentials: true,
   })
 );
